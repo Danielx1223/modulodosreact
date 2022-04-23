0d18648c2ac7f615160bcc979fdd3820b4741dcb
Daniel Nieto <ndanielx@gmail.com>
2022-04-22 19:59:56 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const query = `{
+    productCollection{
+      items{
+              prueba
+              imagenesSubidas {
+                title
+                description
+                contentType
+                fileName
+                size
+                url
+                width
+                height
+              }
+      }
+    }
+    
+  }`; 
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CMS_SPACE_ID}/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authenticate the request
+          Authorization: `Bearer ${process.env.REACT_APP_CMS_TOKEN}`,
+        },
+        // send the GraphQL query
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        // rerender the entire component with new data
+        console.log(data)
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
